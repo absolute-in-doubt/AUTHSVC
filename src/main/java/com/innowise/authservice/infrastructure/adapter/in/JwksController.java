6dbe149a11ce981c9a05fd95c163c116756988fb
AbsoluteInDoubt <pervysbor@gmail.com>
2026-05-31 23:30:48 +0300

    feat(api): set up a JWKS endpoint

---
+++ src/main/java/com/innowise/authservice/infrastructure/adapter/in/JwksController.java
@@ -0,0 +1,25 @@
+package com.innowise.authservice.infrastructure.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nimbusds.jose.jwk.JWKSet;
+
+@RestController
+@RequestMapping("/.well-known")
+public class JwksController {
+
+    private final JWKSet jwkSet;
+
+    public JwksController(JWKSet jwkSet) {
+        this.jwkSet = jwkSet;
+    }
+
+    @GetMapping("/jwks.json")
+    public JWKSet jwks() {
+        return jwkSet;
+    }
+}

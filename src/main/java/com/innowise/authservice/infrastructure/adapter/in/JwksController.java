b9ffc63d29dfffc83f33cd5bc3770fa1428efbfa
AbsoluteInDoubt <pervysbor@gmail.com>
2026-06-16 11:16:21 +0300

    fix(security): fixed JWKS exposure (/jwks endpoint)

--- src/main/java/com/innowise/authservice/infrastructure/adapter/in/JwksController.java
+++ src/main/java/com/innowise/authservice/infrastructure/adapter/in/JwksController.java
@@ -2,11 +2,14 @@ package com.innowise.authservice.infrastructure.adapter.in;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nimbusds.jose.jwk.JWKSet;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/.well-known")
@@ -18,8 +21,8 @@ public class JwksController {
         this.jwkSet = jwkSet;
     }
 
-    @GetMapping("/jwks.json")
-    public JWKSet jwks() {
-        return jwkSet;
+    @GetMapping(value = "/jwks", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> jwks() {
+        return jwkSet.toJSONObject();
     }
 }
